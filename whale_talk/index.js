fea91f29a19c489d40ba22872124455b64c44f2c
khatuna120 <shaveshovak@gmail.com>
2023-12-05 06:51:00 +0100

    add whale talk

--- whale_talk/index.js
+++ whale_talk/index.js
@@ -28,6 +28,22 @@
 // console.log(resultString)
 // // console.log(resultArray)
 
+// Loop Simplification:
+// The original code uses a for loop with an index (inputIndex) to iterate through each character in the input string.
+// In the refactored code, I replaced it with a for...of loop, which directly iterates over the characters of the string. This makes the code cleaner and more readable.
+
+// Condensed Conditions:
+// In the original code, there are separate conditions for checking if a character is 'e' or 'u'. In the refactored code, these conditions are combined into a single if statement using the includes method for the vowels array.
+
+// Simplified Result Array Update:
+// The refactored code pushes the character to the resultArray only if it's a vowel, 'e', or 'u'. This eliminates the need for separate conditions for 'e' and 'u'.
+
+// String Join and Uppercase:
+// After the loop, the code uses join('') to combine the characters in the resultArray into a single string and then applies toUpperCase() to convert the entire string to uppercase.
+
+// Comments and Logging:
+// I kept the comments from the original code that explain the purpose of the loop. I also removed unnecessary logging statements for simplicity.
+
 
 const input = 'turpentine and turtles';
 const vowels = ['a', 'e', 'i', 'o', 'u'];
